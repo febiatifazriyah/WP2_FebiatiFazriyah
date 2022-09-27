8bf217644d9b2b2308a3d351945dc7c1c5f7e4e3
febiatifazriyah <113443545+febiatifazriyah@users.noreply.github.com>
2022-09-27 16:05:18 +0700

    Add files via upload

---
+++ application/views/view-latihan1.php
@@ -0,0 +1,14 @@
+<html>
+<head>
+    <title>Latihan 1</title>
+
+</head>
+<body>
+    Halo kawan.. Yuk kita belajar WEB Programming!<br>
+    Nilai1 = <?= $nilai1; ?><br>
+    Nilai2 = <?= $nilai2; ?><br>
+    Ini hasil dari pemodelan dengan methode penjumlahan yaitu <?=
+$nilai1 . " + " . $nilai2 . " = " . $hasil; ?><br>
+
+</body>
+</html>
