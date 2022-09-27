8025eaa75ab2c7698f9ba311045304fe403fdac0
febiatifazriyah <113443545+febiatifazriyah@users.noreply.github.com>
2022-09-27 11:55:06 +0700

    Add files via upload

---
+++ application/controllers/Contoh1.php
@@ -0,0 +1,11 @@
+<?php
+class Contoh1 extends CI_Controller
+{
+    public function index()
+    {
+        echo "<h1>Perkenalkan</h1>";
+        echo "Nama Saya Febiati Fazriyah - 19210466<br>
+            Saya tinggal di Bekasi<br>
+            Dan saya kuliah di Universitas Bina Sarana Informatika";
+    }
+}
